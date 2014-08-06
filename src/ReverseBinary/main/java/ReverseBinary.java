a8ba0a9c1ca9e686c66c2bfb7e9c49c7c29da331
wtpg <pinny.vooh@gmail.com>
2014-08-06 14:39:27 +0200

    ReverseBinary impl
    Initial commit

---
+++ src/ReverseBinary/main/java/ReverseBinary.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+/**
+ * Reversing the binary representation of a given number. The reverse and convert to binary implementation is done with no use of standard java libs
+ */
+public class ReverseBinary {
+    /**
+     * Converts an integer number into a char array of bits
+     * This implementation is a bit different, comparing to {@link java.lang.Integer#toString(int, int)} as it converts only to the binary representation
+     */
+    protected static char[] toBinary(int number) {
+        char[] res = new char[32]; // int is 32-bit
+        int index = res.length - 1;
+        int mask = 1;
+        do {
+            if ((number & mask) == 0) {
+                res[index--] = '0';
+            } else {
+                res[index--] = '1';
+            }
+
+            number >>>= 1;
+        } while (number > 0);
+
+        // return only significant bits, without leading 0s
+        return Arrays.copyOfRange(res, index + 1, 32);
+    }
+
+    /**
+     * Takes a given number, transforms it to a binary format, reverses a sequence of bites and returns this as a result.
+     */
+    public static int reverse(int number) {
+        // init an array of binaries
+        char[] binary = toBinary(number);
+
+        // reverse
+        int n = binary.length;
+        int j = n / 2;
+        for (int i = 0; i < j; i++) {
+            char tmp = binary[n - j + i];
+            binary[n - j + i] = binary[j - 1 - i];
+            binary[j - 1 - i] = tmp;
+        }
+
+        return Integer.valueOf(new String(binary), 2);
+    }
+}
---
+++ src/ReverseBinary/test/java/ReverseBinaryTest.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+/**
+ * ReverseBinary unit test
+ */
+@RunWith(Parameterized.class)
+public class ReverseBinaryTest {
+    private int input;
+    private char[] binaryForm;
+    private int output;
+
+    @Parameterized.Parameters
+    public static Collection params() {
+        return Arrays.asList(new Object[][]{
+                {13, 11},
+                {128, 1},
+                {47, 61},
+                {589, 713},
+                {0, 0}
+        });
+    }
+
+    public ReverseBinaryTest(int input, int output) {
+        this.input = input;
+        this.binaryForm = Integer.toBinaryString(input).toCharArray();
+        this.output = output;
+    }
+
+    @Test
+    public void testToBinary() throws Exception {
+        assertTrue(Arrays.equals(binaryForm, ReverseBinary.toBinary(input)));
+    }
+
+    @Test
+    public void testReverse() throws Exception {
+        assertEquals(output, ReverseBinary.reverse(input));
+    }
+}
